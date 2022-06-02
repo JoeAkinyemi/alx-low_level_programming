7ca812ee4e5922d20908e4e794f026a43ab3398b
JoeAkinyemi <jonathanakinyemi84@gmail.com>
2022-06-02 10:43:46 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main: the function is ment to print the put function
+ *
+ * decription: from the main function, the code will have to-
+ * print "Programming is like buliding a multilingua puzzle"
+ * retun: 0
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");	
+	return (0);
+
+}	
