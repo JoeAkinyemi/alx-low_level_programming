5b439852c070cfef1543730819db8ced55a5735a
JoeAkinyemi <jonathanakinyemi84@gmail.com>
2022-06-02 10:51:13 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-
 /**
  * main: the function is ment to print the put function
  *
- * decription: from the main function, the code will have to-
- * print "Programming is like buliding a multilingua puzzle"
- * retun: 0
+ *
+ * Retun: 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");	
 	return (0);
-
 }	
