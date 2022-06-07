1b90adf9636fa03b8e0d01c037bcccb6ebaa32cd
JoeAkinyemi <jonathanakinyemi84@gmail.com>
2022-06-07 12:59:14 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio>
+/* main - this function will print _putchar
+ *
+ * Description - Return 0 (Success)
+ */
+void put(void)
+{
+	printf("_putchar\n");
+}
+int main(void)
+{
+	put();
+	return (0);
+}
