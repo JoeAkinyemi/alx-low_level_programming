67b48b812d67488e7ac4af0e2cccfbbdd1266192
JoeAkinyemi <jonathanakinyemi84@gmail.com>
2022-06-24 07:00:22 -0700

    added task

---
+++ 0x0A-agrc_agrv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - print product of argument numbers
+ * argc: argument counter
+ * argv: numbers to multiply
+ *
+ * Return: 0 on success, 1 if two argument not provided
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
