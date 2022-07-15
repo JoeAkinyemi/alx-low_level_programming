22714533d2d07a512c97619b6fce98ff4fe7ae5c
JoeAkinyemi <jonathanakinyemi84@gmail.com>
2022-07-14 21:02:17 -0700

    fix bug

--- 0x12-singly_linked_lists/4-free_lists.c
+++ 0x12-singly_linked_lists/4-free_lists.c
@@ -1,19 +1,16 @@
 #include "lists.h"
 
 /**
- * free_list - frees a list
- * @head: head of the linked list.
- * Return: no return.
+ * free_list - free linked list
+ * @head: linked list
  */
 
 void free_list(list_t *head)
 {
-    list_t *current;
-
-    while ((current = head) != NULL)
-    {
-        head = head->next;
-        free(current->str);
-        free(current);
-    }
+	while (head != NULL)
+	{
+		free(head->str);
+		free(head);
+		head = head->next;
+	}
 }
